32b6f6e695a8a2b76f0957ae881b1a718776f5fb
Emincmg <emin-comoglu@hotmail.com>
2025-02-14 09:29:22 +0300

    removed unneccessary d.i. in event listener

--- src/Listeners/HandlePaymentSuccess.php
+++ src/Listeners/HandlePaymentSuccess.php
@@ -1,6 +1,5 @@
 <?php
 
-use Emincmg\PaymentProcessorLaravel\Services\PaymentService;
 use Emincmg\PaymentProcessorLaravel\Events\PaymentSuccess;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Queue\InteractsWithQueue;
@@ -9,7 +8,7 @@ class HandlePaymentSuccess implements ShouldQueue
 {
     use InteractsWithQueue;
 
-    public function __construct(public PaymentService $paymentService)
+    public function __construct()
     {
     }
 
