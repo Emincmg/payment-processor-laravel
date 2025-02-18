0683974cc28180146f863916ec8ebf53aa6c55cb
Emincmg <emin-comoglu@hotmail.com>
2025-02-18 09:47:07 +0300

    extended publish commands

--- src/Providers/PaymentServiceProvider.php
+++ src/Providers/PaymentServiceProvider.php
@@ -31,12 +31,48 @@ class PaymentServiceProvider extends ServiceProvider
 
     public function boot()
     {
+        // publish config
         $this->publishes([
             __DIR__ . '/../../config/payment.php' => config_path('payment.php'),
         ], 'payment.config');
         $this->publishes([
             __DIR__ . '/../../config/logging.php' => config_path('logging.php'),
         ], 'payment.logging');
+
+        // publish listeners
+        $this->publishes([
+            __DIR__ . '/../Listeners/HandlePaymentError.php' => $this->app()->path() . '/Listeners/HandlePaymentError.php',
+        ], 'payment.listeners');
+        $this->publishes([
+            __DIR__ . '/../Listeners/HandlePaymentSuccess.php' => $this->app()->path() . '/Listeners/HandlePaymentSuccess.php',
+        ], 'payment.listeners');
+
+        // publish DTO's
+        $this->publishes([
+            __DIR__ . '/../Payments/DTO/PaymentRequestDTO.php' => $this->app()->path() . '/DTO/PaymentRequestDTO.php',
+        ], 'payment.dto');
+        $this->publishes([
+            __DIR__ . '/../Payments/DTO/PaymentResponseDTO.php' => $this->app()->path() . '/DTO/PaymentResponseDTO.php',
+        ], 'payment.dto');
+
+        // publish factory
+        $this->publishes([
+            __DIR__ . '/../Payments/Factory/PaymentFactory.php' => $this->app()->path() . '/Factory/PaymentFactory.php',
+        ], 'payment.factory');
+
+        // publish strategies
+        $this->publishes([
+            __DIR__ . '/../Payments/Strategy/PaypalPayment.php' => $this->app()->path() . '/Strategy/PaypalPayment.php',
+        ], 'payment.strategy');
+        $this->publishes([
+            __DIR__ . '/../Payments/Strategy/StripePayment.php' => $this->app()->path() . '/Strategy/StripePayment.php',
+        ], 'payment.strategy');
+
+        // publish exceptions
+        $this->publishes([
+            __DIR__ . '/../Exceptions/PaymentFailedException.php' => $this->app()->path() . '/Exceptions/PaymentFailedException.php',
+        ], 'payment.exceptions');
+
 
         $now = now();
         $this->publishes([
