bba18f17e3e9a007572bd90d2eef93fd25a849cf
Emincmg <emin-comoglu@hotmail.com>
2025-02-18 09:46:27 +0300

    provided methods to the facade docs for IDE

--- src/Facades/PaymentService.php
+++ src/Facades/PaymentService.php
@@ -3,6 +3,14 @@
 namespace Emincmg\LaravelPaymentService\Facades;
 
 use Illuminate\Support\Facades\Facade;
+
+/**
+ * @method static void processPayment ($paymentDTO)
+ * @method static \Emincmg\PaymentProcessorLaravel\Payment returnPaymentInstance ($paymentDTO)
+ * @method static void handleFailure ($payment)
+ * @method static void handleSuccess ($payment)
+ * @method static void associateUser ($user,$payment)
+ */
 class PaymentService extends Facade
 {
     protected static function getFacadeAccessor(): string
